cc8cdad6f1c8ee8f65e437eb3e423d238c28af9e
MarkCNS <commandersalam.ys@gmail.com>
2022-10-12 14:02:18 +0530

    protected routes fix

--- src/routes/privateroutes.js
+++ src/routes/privateroutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import SiteLayout from "../util/layout";
 import LocalStorageService from "../util/localStorageService";
+
 const Privateroutes = () => {
   const isUserLoggedIn = LocalStorageService.getAccessToken();
   return (
